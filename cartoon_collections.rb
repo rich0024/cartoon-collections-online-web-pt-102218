cf152d5be3fa9092bd57fd1ba08d42556fff52e5
Richard Yance <yance0022@gmail.com>
2018-11-04 22:26:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,16 +21,19 @@ def summon_captain_planet(array)# code an argument here
 
 def long_planeteer_calls(arr)# code an argument here
   # Your code here
-  i = 0 
+  i = 0
   if  arr.any? {|i| i.length > 4}
     return true
-  else 
+  else
     return false
   i = i + 1
-  end 
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(lastarr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  lastarr.find do |queso|
+    cheese_types.include?(queso)
+  end
 end
