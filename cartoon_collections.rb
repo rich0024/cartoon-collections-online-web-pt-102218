03d63ac7347c9d2a220fd7e3b0f2620c372b3247
Richard Yance <yance0022@gmail.com>
2018-11-04 22:24:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,15 @@ def summon_captain_planet(array)# code an argument here
     arr
   end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr)# code an argument here
   # Your code here
+  i = 0 
+  if  arr.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese# code an argument here
